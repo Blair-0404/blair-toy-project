c8b174247f1bc716d4b0cfb98aec9cb69ded7ccb
Rachel4858 <rachel458458@gmail.com>
2020-09-04 20:15:33 +0900

    [featture]회원가입 폼중 필수정보부분 마크업 작업: - 로그인페이지, 회원가입 페이지 생성 - 회원가입 페이지에서 필수정보입력부분 마크업 작업

---
+++ blair-toy-project/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './reset.scss'
+import './App.scss'
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
+
+function App() {
+  return (
+    <div className="App">
+      <SignUpPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ blair-toy-project/frontend/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './LoginPage.scss';
+
+function LoginPage() {
+    return (
+        <div className="login-page">로그인 페이지
+        </div>
+    );
+}
+
+export default LoginPage;
